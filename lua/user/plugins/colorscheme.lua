72e7a9d9bf28a10c6e70a390a8e1aedfa010c4b8
uktveris <marius.uktveris@gmail.com>
2024-04-16 16:29:55 +0200

    changed colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -3,20 +3,39 @@
 
 -- alduin
 return {
-    "AlessandroYorba/Alduin",
-    config = function()
-        vim.cmd.colorscheme("alduin")
-        -- changes to transparent background
-        -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-    end
-}
+    -- "AlessandroYorba/Alduin",
+    -- config = function()
+    --     vim.cmd.colorscheme("alduin")
+    --     -- changes to transparent background
+    --     -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+    -- end
 
--- { "rebelot/kanagawa.nvim" },
--- kanagawa
---require("kanagawa").setup({
---    background = {
---        dark = "dragon",
---        light = "lotus"
---    }
---})
---vim.cmd.colorscheme("kanagawa")
+    -- "rebelot/kanagawa.nvim",
+    -- config = function()
+    --     require("kanagawa").setup({
+    --         background = {
+    --             dark = "dragon",
+    --             light = "lotus"
+    --         }
+    --     })
+    --     vim.cmd.colorscheme("kanagawa")
+    -- end
+
+    -- "nyoom-engineering/oxocarbon.nvim",
+    -- config = function()
+    --     vim.opt.background = "dark"
+    --     vim.cmd.colorscheme("oxocarbon")
+    -- end
+
+    -- "Biscuit-Colorscheme/nvim",
+    -- config = function()
+    --     require("biscuit").setup({})
+    --     vim.cmd.colorscheme("biscuit")
+    -- end
+
+    "rose-pine/neovim",
+        config = function()
+            require("rose-pine").setup({})
+            vim.cmd.colorscheme("rose-pine")
+        end
+}
