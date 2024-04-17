d774683d9ea8c5db977c03acaf53a27fb78046e7
uktveris <marius.uktveris@gmail.com>
2024-04-17 11:09:58 +0200

    changed colorscheme

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -3,12 +3,12 @@
 
 -- alduin
 return {
-    -- "AlessandroYorba/Alduin",
-    -- config = function()
-    --     vim.cmd.colorscheme("alduin")
-    --     -- changes to transparent background
-    --     -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-    -- end
+    "AlessandroYorba/Alduin",
+    config = function()
+        vim.cmd.colorscheme("alduin")
+        -- changes to transparent background
+        -- vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+    end
 
     -- "rebelot/kanagawa.nvim",
     -- config = function()
@@ -33,9 +33,26 @@ return {
     --     vim.cmd.colorscheme("biscuit")
     -- end
 
-    "rose-pine/neovim",
-        config = function()
-            require("rose-pine").setup({})
-            vim.cmd.colorscheme("rose-pine")
-        end
+    -- "rose-pine/neovim",
+    --     config = function()
+    --         require("rose-pine").setup({})
+    --         vim.cmd.colorscheme("rose-pine")
+    --     end
+
+    -- "aktersnurra/no-clown-fiesta.nvim",
+    -- config = function()
+    --     require("no-clown-fiesta").setup({})
+    --     vim.cmd.colorscheme("no-clown-fiesta")
+    -- end
+
+    -- "nikolvs/vim-sunbather",
+    -- config = function()
+    --     vim.cmd.colorscheme("sunbather")
+    -- end
+
+    -- "Domeee/mosel.nvim",
+    -- config = function()
+    --     vim.cmd.colorscheme("mosel")
+    -- end
 }
+
