10a651a57f032f4315ddba473789e3080b151435
uktveris <marius.uktveris@gmail.com>
2025-06-08 11:47:34 +0300

    changed to blink.cmp completion engine

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,68 +1,116 @@
 -- AUTOCOMPLETION (CMP) SETUP
 -- =========================
-
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp",
-        "saadparwaiz1/cmp_luasnip",
-        "onsails/lspkind.nvim",
-        {
-            "L3MON4D3/LuaSnip",
-            dependencies = {
-                "rafamadriz/friendly-snippets"
-            }
-        }
-    },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-        local lspkind = require("lspkind")
-        require("luasnip/loaders/from_vscode").lazy_load()
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end
-            },
-            mapping = cmp.mapping.preset.insert({
-                -- previous suggestion
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
-                -- next suggestion
-                ["<C-j>"] = cmp.mapping.select_next_item(),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-1),
-                ["<C-f>"] = cmp.mapping.scroll_docs(1),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    select = false
-                })
-            }),
-            -- sources for autocompletion
-            -- order here matters
-            -- in this case snippets from
-            -- luasnip engine will be suggested first
-            sources = cmp.config.sources({
-                {
-                    name = "luasnip"
-                },
-                {
-                    name = "buffer"
-                },
-                {
-                    name = "path"
-                }
-            }),
-            formatting = {
-                format = lspkind.cmp_format({
-                    maxwidth = 50,
-                    ellipsis_char = "..."
-                })
-            }
-        })
-    end
+  version = "1.*",
+  ---@module "blink.cmp"
+  ---@type blink.cmp.Config
+  opts = {
+    -- "default" (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- "super-tab" for mappings similar to vscode (tab to accept)
+    -- "enter" for enter to accept
+    -- "none" for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { 
+        preset = "enter", 
+
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+      ['<C-j>'] = { 'select_next', 'fallback' },
+    },
+
+    appearance = {
+      nerd_font_variant = "mono"
+    },
+
+    completion = { documentation = { auto_show = true } },
+
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
 }
+
+-- old setup, commented out for now -> using new setup above
+
+-- return {
+--     "hrsh7th/nvim-cmp",
+--     event = "InsertEnter",
+--     dependencies = {
+--         "hrsh7th/cmp-buffer",
+--         "hrsh7th/cmp-path",
+--         "hrsh7th/cmp-nvim-lsp",
+--         "saadparwaiz1/cmp_luasnip",
+--         "onsails/lspkind.nvim",
+--         {
+--             "L3MON4D3/LuaSnip",
+--             dependencies = {
+--                 "rafamadriz/friendly-snippets"
+--             }
+--         }
+--     },
+--     config = function()
+--         local cmp = require("cmp")
+--         local luasnip = require("luasnip")
+--         local lspkind = require("lspkind")
+--         require("luasnip/loaders/from_vscode").lazy_load()
+--
+--         cmp.setup({
+--             snippet = {
+--                 expand = function(args)
+--                     luasnip.lsp_expand(args.body)
+--                 end
+--             },
+--             mapping = cmp.mapping.preset.insert({
+--                 -- previous suggestion
+--                 ["<C-k>"] = cmp.mapping.select_prev_item(),
+--                 -- next suggestion
+--                 ["<C-j>"] = cmp.mapping.select_next_item(),
+--                 ["<C-b>"] = cmp.mapping.scroll_docs(-1),
+--                 ["<C-f>"] = cmp.mapping.scroll_docs(1),
+--                 ["<C-Space>"] = cmp.mapping.complete(),
+--                 ["<C-e>"] = cmp.mapping.abort(),
+--                 ["<CR>"] = cmp.mapping.confirm({
+--                     select = false
+--                 })
+--             }),
+--             -- sources for autocompletion
+--             -- order here matters
+--             -- in this case snippets from
+--             -- luasnip engine will be suggested first
+--             sources = cmp.config.sources({
+--                 {
+--                     name = "luasnip"
+--                 },
+--                 {
+--                     name = "buffer"
+--                 },
+--                 {
+--                     name = "path"
+--                 }
+--             }),
+--             formatting = {
+--                 format = lspkind.cmp_format({
+--                     maxwidth = 50,
+--                     ellipsis_char = "..."
+--                 })
+--             }
+--         })
+--     end
+-- }
--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -2,89 +2,116 @@
 -- ================
 
 return {
-    "neovim/nvim-lspconfig",
-    event = {
-        "BufReadPre",
-        "BufNewFile"
-    },
-    dependencies = {
-        -- "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
-        {
-            "antosha417/nvim-lsp-file-operations",
-            config = true
-        }
-    },
-    config = function()
-        local lspconfig = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        local k = vim.keymap
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("MyLspConfig", {}),
-            callback = function(event)
-                -- buffer options
-                local opts = {
-                    buffer = event.buf,
-                    silent = true
-                }
-                -- show LSP references
-                k.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-                -- go to declaration
-                k.set("n", "gD", vim.lsp.buf.declaration, opts)
-                -- see available code actions, in visual mode will apply to selection
-                k.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                -- smart rename
-                k.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                -- show  diagnostics for file
-                k.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-                -- show diagnostics for line
-                k.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-                -- show documentation for what is under cursor
-                k.set("n", "K", vim.lsp.buf.hover, opts)
-            end
-        })
+  'neovim/nvim-lspconfig',
+  dependencies = { 'saghen/blink.cmp' },
 
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-
-        local signs = {
-            Error = "",
-            Warning = "",
-            Hint = "",
-            Info = "󰋼"
-        }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, {
-                text = icon,
-                texthl = hl,
-                numhl = ""
-            })
-        end
-
-        mason_lspconfig.setup()
-
-        local servers = mason_lspconfig.get_installed_servers()
-
-        for _, server_name in ipairs(servers) do
-            local opts = {
-                capabilities = capabilities
-            }
-
-            if server_name == "lua_ls" then
-                opts.settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {"vim"}
-                        },
-                        completion = {
-                            callSnippet = "Replace"
-                        },
-                    },
-                }
-            end
-
-            lspconfig[server_name].setup(opts)
-        end
+  -- example using `opts` for defining servers
+  opts = {
+    servers = {
+      lua_ls = {}
+    }
+  },
+  config = function(_, opts)
+    local lspconfig = require('lspconfig')
+    for server, config in pairs(opts.servers) do
+      -- passing config.capabilities to blink.cmp merges with the capabilities in your
+      -- `opts[server].capabilities, if you've defined it
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      lspconfig[server].setup(config)
     end
+  end
+
+ -- example calling setup directly for each LSP
+  -- config = function()
+  --   local capabilities = require('blink.cmp').get_lsp_capabilities()
+  --   local lspconfig = require('lspconfig')
+  --
+  --   lspconfig['lua_ls'].setup({ capabilities = capabilities })
+  -- end
 }
+
+
+-- old setup, using new one above
+
+-- return {
+--     "neovim/nvim-lspconfig",
+--     event = {
+--         "BufReadPre",
+--         "BufNewFile"
+--     },
+--     dependencies = {
+--         "hrsh7th/cmp-nvim-lsp",
+--     },
+--     config = function()
+--         local lspconfig = require("lspconfig")
+--         local mason_lspconfig = require("mason-lspconfig")
+--         local cmp_nvim_lsp = require("cmp_nvim_lsp")
+--         local k = vim.keymap
+--         vim.api.nvim_create_autocmd("LspAttach", {
+--             group = vim.api.nvim_create_augroup("MyLspConfig", {}),
+--             callback = function(event)
+--                 -- buffer options
+--                 local opts = {
+--                     buffer = event.buf,
+--                     silent = true
+--                 }
+--                 -- show LSP references
+--                 k.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+--                 -- go to declaration
+--                 k.set("n", "gD", vim.lsp.buf.declaration, opts)
+--                 -- see available code actions, in visual mode will apply to selection
+--                 k.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+--                 -- smart rename
+--                 k.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+--                 -- show  diagnostics for file
+--                 k.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+--                 -- show diagnostics for line
+--                 k.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+--                 -- show documentation for what is under cursor
+--                 k.set("n", "K", vim.lsp.buf.hover, opts)
+--             end
+--         })
+--
+--         local capabilities = cmp_nvim_lsp.default_capabilities()
+--
+--         local signs = {
+--             Error = "",
+--             Warning = "",
+--             Hint = "",
+--             Info = "󰋼"
+--         }
+--         for type, icon in pairs(signs) do
+--             local hl = "DiagnosticSign" .. type
+--             vim.fn.sign_define(hl, {
+--                 text = icon,
+--                 texthl = hl,
+--                 numhl = ""
+--             })
+--         end
+--
+--         mason_lspconfig.setup()
+--
+--         local servers = mason_lspconfig.get_installed_servers()
+--
+--         for _, server_name in ipairs(servers) do
+--             local opts = {
+--                 capabilities = capabilities
+--             }
+--
+--             if server_name == "lua_ls" then
+--                 opts.settings = {
+--                     Lua = {
+--                         diagnostics = {
+--                             globals = {"vim"}
+--                         },
+--                         completion = {
+--                             callSnippet = "Replace"
+--                         },
+--                     },
+--                 }
+--             end
+--
+--             lspconfig[server_name].setup(opts)
+--         end
+--     end
+-- }
